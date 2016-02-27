40af6c48fa0fe984c39fb012276a6710e66eb1fe
kushal <kushaldeveloper@gmail.com>
2016-02-27 12:48:38 -0500

    move documentation to root

--- Makefile
+++
@@ -1,2 +0,0 @@
-LIBROOT := ..
-include $(LIBROOT)/crow-lib/Makefile
--- unicorn/string.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "unicorn/string-algorithm.hpp"
-#include "unicorn/string-case.hpp"
-#include "unicorn/string-conversion.hpp"
-#include "unicorn/string-escape.hpp"
-#include "unicorn/string-manip.hpp"
-#include "unicorn/string-property.hpp"
-#include "unicorn/string-size.hpp"
